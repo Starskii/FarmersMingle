e50f953846fe5437f741bcec78fcb86188481f25
Jacob <jakeylakie@yahoo.com>
2023-08-30 14:52:32 -0700

    add lb

--- farmers-mingle/src/views/DiscordView.vue
+++ farmers-mingle/src/views/DiscordView.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="about">
-    <h1>Join our Discord!</h1>
-    <br>
-    <p>Join our discord by clicking <a href="https://discord.gg/dBYRUvfuMP">here!</a></p>
-  </div>
+  <WelcomeItem>
+    <template #icon>
+    <DocumentationIcon />
+    </template>
+    <template #heading>Join our Discord!</template>
+    Join our discord by clicking <a href="https://discord.gg/dBYRUvfuMP">here!</a>
+  </WelcomeItem>
 </template>
+
+
 
 <style>
 @media (min-width: 1024px) {
