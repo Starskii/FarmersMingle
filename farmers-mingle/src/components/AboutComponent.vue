5e43d6bdf34fea80610c6cdbce94842d6676f725
Jacob <jakeylakie@yahoo.com>
2023-09-05 18:29:55 -0700

    added onMounted

--- farmers-mingle/src/components/AboutComponent.vue
+++ farmers-mingle/src/components/AboutComponent.vue
@@ -5,9 +5,18 @@ import ToolingIcon from "./icons/IconTooling.vue";
 import EcosystemIcon from "./icons/IconEcosystem.vue";
 import CommunityIcon from "./icons/IconCommunity.vue";
 import SupportIcon from "./icons/IconSupport.vue";
-import { ref } from 'vue';
+import { ref, watchEffect, onMounted } from 'vue'
+
+const API_URL = 'https://api.clashofclans.com/v1/clans/2VYVGV08P/members';
+const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjI5NDMyMzIxLWQ2YjItNGQ0Zi1iMTIzLTI3ZWIxZmY5YWE0MyIsImlhdCI6MTY5Mzk2MjEzNSwic3ViIjoiZGV2ZWxvcGVyLzdhMjcwZDgyLTlhNTctYjMwZS0xYWZkLTYyMDNiNzBlZjFjMSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwMy4yMjQuMjEyLjIyMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.SHn6SgznwJE6CfTQ-bXIrzTIqSVeDsJ8cJunl7SUpvvGT04JSahCEK-RBeZM2AlFauT14aadM1cgXO8g4AkN0A';
 
 const count = ref(0);
+
+// curl -H 'Authorization: ' 
+
+function onMounted () {
+  console.log('Hello World');
+}
 
 function increment() {
   count.value++;
@@ -26,3 +35,4 @@ function increment() {
     <button @click="increment">Increment</button>
   </WelcomeItem>
 </template>
+
