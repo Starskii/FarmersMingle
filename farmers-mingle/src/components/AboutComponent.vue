ff2f0e5d012650ab82e6c237c1497057b62cb6c9
Jacob <jakeylakie@yahoo.com>
2023-09-05 17:29:25 -0700

    added reactivity

---
+++ farmers-mingle/src/components/AboutComponent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import ToolingIcon from "./icons/IconTooling.vue";
+import EcosystemIcon from "./icons/IconEcosystem.vue";
+import CommunityIcon from "./icons/IconCommunity.vue";
+import SupportIcon from "./icons/IconSupport.vue";
+import { ref } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>About our Clan</template>
+      <p>Count: {{ count }}</p>
+    <button @click="increment">Increment</button>
+  </WelcomeItem>
+</template>
