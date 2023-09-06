e348a34d89ab15df43a34b52d242dbdf3b46653c
Jacob <jakeylakie@yahoo.com>
2023-09-05 19:30:56 -0700

    fixed?

--- farmers-mingle/src/App.vue
+++ farmers-mingle/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
-import { inject } from '@vercel/analytics';
+import { inject } from 'vercel/analytics';
  
 inject();
 </script>
