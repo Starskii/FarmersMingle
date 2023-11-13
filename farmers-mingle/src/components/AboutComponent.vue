b3401f3dd78cf6aff93e2b2a29a041b294691f91
Jacob <jakeylakie@yahoo.com>
2023-11-13 08:50:25 -0800

    adding firestore integration

--- farmers-mingle/src/components/AboutComponent.vue
+++ farmers-mingle/src/components/AboutComponent.vue
@@ -8,22 +8,14 @@ import SupportIcon from "./icons/IconSupport.vue";
 import { ref, watchEffect, onMounted } from 'vue'
 import axios from 'axios';
 
-const API_URL = 'https://api.clashofclans.com/v1/clans/2VYVGV08P/members';
-const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjI5NDMyMzIxLWQ2YjItNGQ0Zi1iMTIzLTI3ZWIxZmY5YWE0MyIsImlhdCI6MTY5Mzk2MjEzNSwic3ViIjoiZGV2ZWxvcGVyLzdhMjcwZDgyLTlhNTctYjMwZS0xYWZkLTYyMDNiNzBlZjFjMSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwMy4yMjQuMjEyLjIyMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.SHn6SgznwJE6CfTQ-bXIrzTIqSVeDsJ8cJunl7SUpvvGT04JSahCEK-RBeZM2AlFauT14aadM1cgXO8g4AkN0A';
-const CONFIG = {
-  headers:{
-    Authorization: API_KEY,
-  }
-};
+
 
 const members = ref(null);
 
 // curl -H 'Authorization: ' 
 
 onMounted(async () => {
-      const res = await axios.get(API_URL, CONFIG);
-      members.value = res.data;
-      console.log(res);
+    console.log('hello world');
     });
 
 
