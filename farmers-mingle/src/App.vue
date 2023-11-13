7e17da686c3754d2e094c2a833573581b5e11efb
Jacob <jakeylakie@yahoo.com>
2023-11-13 09:44:50 -0800

    maybe work

--- farmers-mingle/src/App.vue
+++ farmers-mingle/src/App.vue
@@ -2,27 +2,27 @@
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 import { inject } from '@vercel/analytics';
- // Import the functions you need from the SDKs you need
-import { initializeApp } from "@firebase/app";
-import { getAnalytics } from "@firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCezsdCdbzomTnUYvikiom6MLG233mTMkE",
-  authDomain: "farmersmingle.firebaseapp.com",
-  projectId: "farmersmingle",
-  storageBucket: "farmersmingle.appspot.com",
-  messagingSenderId: "963512979321",
-  appId: "1:963512979321:web:03e6ae8b7164af6e62a9a8",
-  measurementId: "G-39VXQCQEVF"
-};
-
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+ import { getAnalytics } from "firebase/analytics";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+ // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+   apiKey: "AIzaSyCezsdCdbzomTnUYvikiom6MLG233mTMkE",
+   authDomain: "farmersmingle.firebaseapp.com",
+   projectId: "farmersmingle",
+   storageBucket: "farmersmingle.appspot.com",
+   messagingSenderId: "963512979321",
+   appId: "1:963512979321:web:03e6ae8b7164af6e62a9a8",
+   measurementId: "G-39VXQCQEVF"
+ };
+ 
+ 
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ const analytics = getAnalytics(app);
 
 inject();
 </script>
--- farmers-mingle/src/main.ts
+++ farmers-mingle/src/main.ts
@@ -2,27 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "firebase/app";
- import { getAnalytics } from "firebase/analytics";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
- // Your web app's Firebase configuration
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-   apiKey: "AIzaSyCezsdCdbzomTnUYvikiom6MLG233mTMkE",
-   authDomain: "farmersmingle.firebaseapp.com",
-   projectId: "farmersmingle",
-   storageBucket: "farmersmingle.appspot.com",
-   messagingSenderId: "963512979321",
-   appId: "1:963512979321:web:03e6ae8b7164af6e62a9a8",
-   measurementId: "G-39VXQCQEVF"
- };
- 
- 
- // Initialize Firebase
- const fapp = initializeApp(firebaseConfig);
- const analytics = getAnalytics(fapp);
 
 const app = createApp(App);
 
