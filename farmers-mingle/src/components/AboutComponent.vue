6c016752f56020e3179b9df3508b8bba39e59785
Jacob <jakeylakie@yahoo.com>
2023-11-15 17:16:16 -0800

    mod table

--- farmers-mingle/src/components/AboutComponent.vue
+++ farmers-mingle/src/components/AboutComponent.vue
@@ -45,7 +45,7 @@ onMounted(async () => {
         </tr>
         <tr v-for="member in members.values()">
           <td>
-            <RouterLink :to="'/about/players/' + member.tag.replace("#", "%23")">{{ member.name }}</RouterLink>
+            <RouterLink :to="'/about/players/' + member.tag.replace('#', '%23')">{{ member.name }}</RouterLink>
           </td>
           <td>
             {{ member.role }}
