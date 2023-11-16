5a21a2abeff21c8d04092538ef621aec6ceaeb1a
Jacob <jakeylakie@yahoo.com>
2023-11-15 21:09:27 -0800

    added style

--- farmers-mingle/src/components/Leaderboard.vue
+++ farmers-mingle/src/components/Leaderboard.vue
@@ -14,7 +14,7 @@ const clanRef = collection(db, "clans");
 const tblHeaders = ref(['Name', 'Role', 'Trophies'])
 const members = ref<Player[]>([]);
 const clanName = ref(null);
-const loaded = ref(false) 
+const loaded = ref(false)
 
 onMounted(async () => {
   const snapshot = await getDocs(clanRef);
@@ -39,10 +39,13 @@ onMounted(async () => {
       <DocumentationIcon />
     </template>
     <template #heading>{{ clanName }}</template>
-      <table>
+    <table class="styled-table">
+      <thead>
         <tr>
-          <td v-for="header in tblHeaders.values()"> {{ header }} </td>
+          <th v-for="header in tblHeaders.values()"> {{ header }} </th>
         </tr>
+      </thead>
+      <tbody>
         <tr v-for="member in members.values()">
           <td>
             <RouterLink :to="'/stats/players/' + member.tag.replace('#', '%23')">{{ member.name }}</RouterLink>
@@ -54,7 +57,47 @@ onMounted(async () => {
             {{ member.trophies }}
           </td>
         </tr>
-      </table>
+      </tbody>
+    </table>
   </WelcomeItem>
 </template>
 
+<style>
+.styled-table {
+    border-collapse: collapse;
+    margin: 25px 0;
+    font-size: 0.9em;
+    font-family: sans-serif;
+    min-width: 400px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+
+.styled-table thead tr {
+    background-color: #009879;
+    color: #ffffff;
+    text-align: left;
+}
+
+.styled-table th,
+.styled-table td {
+    padding: 12px 15px;
+}
+
+.styled-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+    border-bottom: 2px solid #009879;
+}
+
+.styled-table tbody tr:hover {
+    font-weight: bold;
+    color: #009879;
+}
+
+</style>
