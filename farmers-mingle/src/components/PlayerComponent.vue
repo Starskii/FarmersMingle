cc3bda6ab468d753dd626de48833cc57a4ddbe25
Jacob <jakeylakie@yahoo.com>
2023-11-20 14:13:20 -0800

    added chart back

--- farmers-mingle/src/components/PlayerComponent.vue
+++ farmers-mingle/src/components/PlayerComponent.vue
@@ -19,12 +19,18 @@ const warPreference = ref(null);
 const donations = ref(null);
 const donationsReceived = ref(null);
 const clanCapitalContributions = ref(null);
+const options = ref({
+chart: {
+    id: 'vuechart-example'
+},
+xaxis: {
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+}
+});
 const series = ref([{
-        name: 'series-1',
-        data: [30, 40, 45, 50, 49, 60, 70, 91]
-      }]);
-
-const options = ref({});
+name: 'series-1',
+data: [30, 40, 45, 50, 49, 60, 70, 91]
+}]);
 
 onMounted(async () => {
     const playerId = router.currentRoute.value.path.split("/")[3].replace("%23", "#");
